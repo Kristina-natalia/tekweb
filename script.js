114c5f86905bf919bc91368e00d832b8617b344b
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-09-22 08:06:12 +0800

    Update script.js
    Signed-off-by: Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,109 +1,94 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const appContainer = document.querySelector(".app-container");
+    const fibonacciInput = document.getElementById("fibonacciInput");
+    const calculateFibonacciButton = document.getElementById("calculateFibonacci");
+    const fibonacciResult = document.getElementById("fibonacciResult");
 
-    // Bagian Deret Fibonacci
-    const fibonacciInput = document.getElementById("fibonacci-input");
-    const fibonacciButton = document.getElementById("fibonacci-button");
-    const fibonacciResult = document.getElementById("fibonacci-result");
-
-    fibonacciButton.addEventListener("click", function () {
+    calculateFibonacciButton.addEventListener("click", function () {
         const n = parseInt(fibonacciInput.value);
         if (!isNaN(n)) {
             const result = calculateFibonacci(n);
-            fibonacciResult.textContent = "Hasil: " + result;
+            fibonacciResult.textContent = `Deret Fibonacci ke-${n}: ${result}`;
         }
     });
 
-    // Bagian Volume Bangun Ruang
-    const shapeSelect = document.getElementById("shape-select");
-    const shapeInputs = document.getElementById("shape-inputs");
-    const volumeButton = document.getElementById("volume-button");
-    const volumeResult = document.getElementById("volume-result");
+    const shapeSelect = document.getElementById("shapeSelect");
+    const shapeInputs = document.getElementById("shapeInputs");
+    const shapeInput1 = document.getElementById("shapeInput1");
+    const shapeInput2 = document.getElementById("shapeInput2");
+    const shapeInput3 = document.getElementById("shapeInput3");
+    const shapeInput3Label = document.getElementById("shapeInput3Label");
+    const calculateVolumeButton = document.getElementById("calculateVolume");
+    const volumeResult = document.getElementById("volumeResult");
 
     shapeSelect.addEventListener("change", function () {
         const selectedShape = shapeSelect.value;
-        shapeInputs.innerHTML = "";
-
-        if (selectedShape === "kubus") {
-            const kubusSisi = createInput("kubus-sisi", "Masukkan panjang sisi:");
-            shapeInputs.appendChild(kubusSisi);
-        } else if (selectedShape === "balok") {
-            const balokPanjang = createInput("balok-panjang", "Masukkan panjang:");
-            const balokLebar = createInput("balok-lebar", "Masukkan lebar:");
-            const balokTinggi = createInput("balok-tinggi", "Masukkan tinggi:");
-
-            shapeInputs.appendChild(balokPanjang);
-            shapeInputs.appendChild(balokLebar);
-            shapeInputs.appendChild(balokTinggi);
-        } else if (selectedShape === "segitiga") {
-            const segitigaAlas = createInput("segitiga-alas", "Masukkan alas:");
-            const segitigaTinggi = createInput("segitiga-tinggi", "Masukkan tinggi:");
-
-            shapeInputs.appendChild(segitigaAlas);
-            shapeInputs.appendChild(segitigaTinggi);
-        } else if (selectedShape === "tabung") {
-            const tabungJari = createInput("tabung-jari", "Masukkan jari-jari:");
-            const tabungTinggi = createInput("tabung-tinggi", "Masukkan tinggi:");
-
-            shapeInputs.appendChild(tabungJari);
-            shapeInputs.appendChild(tabungTinggi);
-        }
-    });
-
-    volumeButton.addEventListener("click", function () {
-        const selectedShape = shapeSelect.value;
-        const shapeInputsArray = Array.from(shapeInputs.getElementsByTagName("input"));
-        const validInputs = shapeInputsArray.every(input => !isNaN(parseFloat(input.value)));
-
-        if (validInputs) {
-            let result = 0;
-
-            if (selectedShape === "kubus") {
-                const sisi = parseFloat(document.getElementById("kubus-sisi").value);
-                result = sisi ** 3;
-            } else if (selectedShape === "balok") {
-                const panjang = parseFloat(document.getElementById("balok-panjang").value);
-                const lebar = parseFloat(document.getElementById("balok-lebar").value);
-                const tinggi = parseFloat(document.getElementById("balok-tinggi").value);
-                result = panjang * lebar * tinggi;
-            } else if (selectedShape === "segitiga") {
-                const alas = parseFloat(document.getElementById("segitiga-alas").value);
-                const tinggi = parseFloat(document.getElementById("segitiga-tinggi").value);
-                result = 0.5 * alas * tinggi;
-            } else if (selectedShape === "tabung") {
-                const jari = parseFloat(document.getElementById("tabung-jari").value);
-                const tinggi = parseFloat(document.getElementById("tabung-tinggi").value);
-                result = Math.PI * jari ** 2 * tinggi;
+        if (selectedShape === "none") {
+            shapeInputs.classList.add("hidden");
+            shapeInput3Label.classList.add("hidden");
+            shapeInput3.classList.add("hidden");
+        } else {
+            shapeInputs.classList.remove("hidden");
+            if (selectedShape === "triangle") {
+                shapeInput3Label.classList.remove("hidden");
+                shapeInput3.classList.remove("hidden");
+            } else {
+                shapeInput3Label.classList.add("hidden");
+                shapeInput3.classList.add("hidden");
             }
-
-            volumeResult.textContent = "Hasil: " + result.toFixed(2);
         }
     });
 
-    // Helper Functions
-    function createInput(id, label) {
-        const inputLabel = document.createElement("label");
-        inputLabel.htmlFor = id;
-        inputLabel.textContent = label;
-        const input = document.createElement("input");
-        input.type = "number";
-        input.id = id;
-        return inputLabel;
-    }
+    calculateVolumeButton.addEventListener("click", function () {
+        const selectedShape = shapeSelect.value;
+        let volume;
+
+        const value1 = parseFloat(shapeInput1.value);
+        const value2 = parseFloat(shapeInput2.value);
+        const value3 = parseFloat(shapeInput3.value);
+
+        if (selectedShape === "cube") {
+            volume = calculateVolumeCube(value1);
+        } else if (selectedShape === "rectangularPrism") {
+            volume = calculateVolumeRectangularPrism(value1, value2, value3);
+        } else if (selectedShape === "cylinder") {
+            volume = calculateVolumeCylinder(value1, value2);
+        } else if (selectedShape === "triangle") {
+            volume = calculateVolumeTriangle(value1, value2, value3);
+        }
+
+        if (!isNaN(volume)) {
+            volumeResult.textContent = `Volume: ${volume}`;
+        }
+    });
 
     function calculateFibonacci(n) {
-        if (n <= 0) return 0;
-        if (n === 1) return 1;
-
-        let prev = 0;
-        let current = 1;
-
-        for (let i = 2; i <= n; i++) {
-            const next = prev + current;
-            prev = current;
-            current = next;
+        if (n <= 1) {
+            return n;
         }
+        let a = 0;
+        let b = 1;
+        let temp;
+        for (let i = 2; i <= n; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 
-        return current;
+    function calculateVolumeCube(sisi) {
+        return sisi ** 3;
+    }
+
+    function calculateVolumeRectangularPrism(panjang, lebar, tinggi) {
+        return panjang * lebar * tinggi;
+    }
+
+    function calculateVolumeCylinder(jariJari, tinggi) {
+        return Math.PI * jariJari ** 2 * tinggi;
+    }
+
+    function calculateVolumeTriangle(alas, tinggi, panjang) {
+        return (1 / 2) * alas * tinggi * panjang;
     }
 });
